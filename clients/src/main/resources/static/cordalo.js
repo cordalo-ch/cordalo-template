ea56a86e7a152b21e2a7482d27ddad2b72e0545f
Lorenz Hänggi <lolo8304@gmail.com>
2019-11-04 12:36:10 +0100

    refactor to cordalo.js

---
+++ clients/src/main/resources/static/cordalo.js
@@ -0,0 +1,91 @@
+var cordaloEnv = (function() {
+    function _GETvar(parameterName) {
+        var result = null,
+        tmp = [];
+        location.search
+            .substr(1)
+            .split("&")
+            .forEach(function (item) {
+                tmp = item.split("=");
+                if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+        return result;
+    };
+    function _GetMainUrl() {
+        if (port != null) {
+            if (local != null) {
+                return "http://localhost:"+port
+            } else {
+                return document.location.protocol + "//" + document.location.hostname + ":" + port
+            }
+        } else {
+            return document.location.protocol + "//" + document.location.hostname + ":" + document.location.port
+        }
+    }
+    function init() {
+        jQuery.fn.fail = function(f) {
+            var o = $(this[0]) // This is the element
+            f("missing mock for "+o);
+            return this; // This is needed so other functions can keep chaining off of this
+        };
+    }
+    var port = _GETvar("port");
+    var local = _GETvar("local");
+    var mock = _GETvar("mock");
+    var MAIN_URL = _GetMainUrl();
+    var MOCK_DATA = [];
+    init();
+
+    return {
+        API_URL: function(file) {
+            return (file != null) ? MAIN_URL + file : MAIN_URL
+        },
+        addMock: function(url, data) {
+            MOCK_DATA[url] = data;
+        },
+        findAPI: function(url) {
+            const regex = /http[s]?\:\/\/[^\/]*\:?[0-9]*(\/.*)/gs;
+            let m;
+            var resultMatch = url;
+
+            while ((m = regex.exec(url)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === regex.lastIndex) {
+                    regex.lastIndex++;
+                }
+
+                // The result can be accessed through the `m`-variable.
+                m.forEach((match, groupIndex) => {
+                    if (match != url) {
+                        resultMatch = match;
+                        return match;
+                    }
+                });
+            }
+            return resultMatch;
+        },
+        jQuery_get: function(object) {
+            if (mock) {
+                var api = cordaloEnv.findAPI(object.url);
+                if (api && MOCK_DATA[api]) {
+                    console.log("successful MOCK_DATA for url "+object.url);
+                    object.success(MOCK_DATA[api]);
+                } else {
+                    console.log("missing MOCK_DATA for url "+object.url);
+                }
+                return $(API_Failed());
+            }
+            return $.get(object);
+        }
+    };
+})();
+
+
+function API_Failed() {
+    this.f;
+}
+API_Failed.prototype.fail = function(f) {
+    this.f = f;
+}
+
+$get=cordaloEnv.jQuery_get;
--- clients/src/main/resources/static/visualization.js
+++ clients/src/main/resources/static/visualization.js
@@ -1,42 +1,7 @@
-
-function GETvar(parameterName) {
-    var result = null,
-    tmp = [];
-    location.search
-        .substr(1)
-        .split("&")
-        .forEach(function (item) {
-            tmp = item.split("=");
-            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-    });
-    return result;
-}
-
-var port = GETvar("port");
-var local = GETvar("local");
-MAIN_URL="";
-if (port != null) {
-    if (local != null) {
-        MAIN_URL = "http://localhost:"+port
-    } else {
-        MAIN_URL = document.location.protocol + "//" + document.location.hostname + ":" + port
-    }
-   //MAIN_URL="http://localhost:"+port;
-}
-
-//var toRefresh = true;
-var toRefresh = false;
-function refreshGrids() {
-    if (toRefresh) {
-        $( "#runningAnimation" ).hide();
-        history.go(0)
-    } else {
-    }
-}
-function forceRefreshGrids() {
-    toRefresh = true;
-    //toRefresh = false;
-    refreshGrids();
+if (cordaloEnv) {
+    console.log("cordalo connected to "+cordaloEnv.API_URL())
+} else {
+    console.log("load cordalo.js in your html page");
 }
 
 function animationOff() {
@@ -46,12 +11,11 @@ function animationOn() {
     setWebSocketConnected(true, true);
 }
 
-
 function createNewService(self, service, spShort, price, optionalIds){
     animationOn();
     $.ajax(
         {
-            url: MAIN_URL+"/api/v1/cordalo/template/services/",
+            url: cordaloEnv.API_URL("/api/v1/cordalo/template/services/"),
             method: "POST",
             headers: {
                 "Content-Type" : "application/x-www-form-urlencoded"
@@ -59,10 +23,8 @@ function createNewService(self, service, spShort, price, optionalIds){
             data: "service-name="+encodeURI(service)+"&data="+encodeURI(getServiceData())+"&price="+encodeURI(price)
         }
     ).done(function(result) {
-
     }).fail(function(jqXHR, textStatus) {
         alert(jqXHR.responseText);
-        forceRefreshGrids();
     });
 }
 
@@ -72,7 +34,7 @@ function deleteService(service) {
         animationOn();
         $.ajax(
             {
-                url: MAIN_URL+"/api/v1/cordalo/template/services/"+id,
+                url: cordaloEnv.API_URL("/api/v1/cordalo/template/services/"+id),
                 method: "DELETE",
                 headers: {
                     "Content-Type" : "application/x-www-form-urlencoded"
@@ -84,7 +46,6 @@ function deleteService(service) {
             animationOff();
         }).fail(function(jqXHR, textStatus) {
             alert(jqXHR.responseText);
-            forceRefreshGrids();
         });
     }
 }
@@ -108,43 +69,41 @@ function makeOptions(id, list) {
 }
 function onSelectionChanged(select) {
     if ($(select).val() != '') {
-            var url = $(select).val();
-            var action = url.split("/").reverse()[0];
-            if (action != "SHARE") {
-                animationOn();
-                $.ajax(
-                    {
-                        url: url,
-                        method: "POST",
-                        headers: {
-                            "Content-Type" : "application/x-www-form-urlencoded"
-                        },
-                        data: ""
-                    }
-                ).done(function(result) {
+        var url = $(select).val();
+        var action = url.split("/").reverse()[0];
+        if (action != "SHARE") {
+            animationOn();
+            $.ajax(
+                {
+                    url: url,
+                    method: "POST",
+                    headers: {
+                        "Content-Type" : "application/x-www-form-urlencoded"
+                    },
+                    data: ""
+                }
+            ).done(function(result) {
 
-                }).fail(function(jqXHR, textStatus) {
-                    alert(jqXHR.responseText);
-                    forceRefreshGrids();
-                });
-            } else {
-                animationOn();
-                $.ajax(
-                    {
-                        url: url,
-                        method: "POST",
-                        headers: {
-                            "Content-Type" : "application/x-www-form-urlencoded"
-                        },
-                        data: "service-provider="+encodeURI(ME_RANDOM_PEER)
-                    }
-                ).done(function(result) {
+            }).fail(function(jqXHR, textStatus) {
+                alert(jqXHR.responseText);
+            });
+        } else {
+            animationOn();
+            $.ajax(
+                {
+                    url: url,
+                    method: "POST",
+                    headers: {
+                        "Content-Type" : "application/x-www-form-urlencoded"
+                    },
+                    data: "service-provider="+encodeURI(ME_RANDOM_PEER)
+                }
+            ).done(function(result) {
 
-                }).fail(function(jqXHR, textStatus) {
-                    alert(jqXHR.responseText);
-                    forceRefreshGrids();
-                });            }
-
+            }).fail(function(jqXHR, textStatus) {
+                alert(jqXHR.responseText);
+            });
+        }
     }
 };
 
@@ -190,7 +149,7 @@ function show_services(tagName, result) {
                 }
             },
             { title: "Link", name: "state.id", type: "text", align: "center", width: 30, itemTemplate: function(value) {
-                 var res = "<a target='_blank' href='"+MAIN_URL+"/api/v1/cordalo/template/services/"+value.id+"'>o</a>&nbsp;"
+                 var res = "<a target='_blank' href='"+cordaloEnv.API_URL("/api/v1/cordalo/template/services/"+value.id)+")'>o</a>&nbsp;"
                     +"<a value="+value.id+" href=\"#\" onClick=\"deleteService(this)\"'>X</a>";
                  i = i + 10;
                 return strongS(i)+res+strongE(i); }
@@ -229,7 +188,7 @@ function getRandomInt(max) {
 
 function get_me() {
     $get({
-        url: MAIN_URL+"/api/v1/cordalo/template/me",
+        url: cordaloEnv.API_URL("/api/v1/cordalo/template/me"),
         data: {        },
         success: function( result ) {
             var x500name = result.me.x500Principal.name.split(",");
@@ -248,7 +207,7 @@ function get_me() {
 
 function get_peers() {
     $get({
-        url: MAIN_URL+"/api/v1/cordalo/template/peers",
+        url: cordaloEnv.API_URL("/api/v1/cordalo/template/peers"),
         data: {        },
         success: function( result ) {
             ME_RANDOM_PEER = result.peers[getRandomInt(result.peers.length)].x500Principal.name;
@@ -258,7 +217,7 @@ function get_peers() {
 
 function get_services() {
     $get({
-        url: MAIN_URL+"/api/v1/cordalo/template/services",
+        url: cordaloEnv.API_URL("/api/v1/cordalo/template/services"),
         data: { },
         success: function( result ) {
             show_services("#services-template", result);
@@ -278,7 +237,7 @@ function setWebSocketConnected(connected, running) {
 
 
 function connectWebSocket() {
-    var socket = new SockJS(MAIN_URL+'/gs-guide-websocket');
+    var socket = new SockJS(cordaloEnv.API_URL("/gs-guide-websocket"));
     stompClient = Stomp.over(socket);
     stompClient.debug = null;
     stompClient.connect({}, function (frame) {
@@ -302,58 +261,17 @@ $(document).ready(function(){
 
 });
 
-function findAPI(url) {
-    const regex = /http[s]?\:\/\/[^\/]*\:?[0-9]*(\/.*)/gs;
-    let m;
 
-    while ((m = regex.exec(url)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-            regex.lastIndex++;
-        }
+cordaloEnv.addMock(
+    "/api/v1/cordalo/template/services",
+    [{"state":{"id":{"externalId":null,"id":"3a3fe715-ef9e-4683-a932-7f67b2afbac0"},"state":"SHARED","serviceName":"Tele Medicine","serviceData":{"test":"42"},"price":25,"initiatorX500":"O=Company-B,L=Winterthur,ST=ZH,C=CH","serviceProviderX500":"O=Company-D,L=Zurich,ST=ZH,C=CH","linearId":{"externalId":null,"id":"3a3fe715-ef9e-4683-a932-7f67b2afbac0"}},"links":{"UPDATE":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/UPDATE","WITHDRAW":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/WITHDRAW","SEND_PAYMENT":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/SEND_PAYMENT","ACCEPT":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/ACCEPT","DECLINE":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/DECLINE","self":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0"},"error":null},{"state":{"id":{"externalId":null,"id":"fc6f8a5f-dee3-48ad-a245-594e1b631cc9"},"state":"SHARED","serviceName":"New Service","serviceData":{},"price":34,"initiatorX500":"O=Company-A,L=Zurich,ST=ZH,C=CH","serviceProviderX500":"O=Company-D,L=Zurich,ST=ZH,C=CH","linearId":{"externalId":null,"id":"fc6f8a5f-dee3-48ad-a245-594e1b631cc9"}},"links":{"UPDATE":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/UPDATE","WITHDRAW":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/WITHDRAW","SEND_PAYMENT":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/SEND_PAYMENT","ACCEPT":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/ACCEPT","DECLINE":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/DECLINE","self":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9"},"error":null}]
+);
 
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            return match;
-        });
-    }
-    return url;
-}
-
-MOCK = {
-    "/api/v1/cordalo/template/services" : [{"state":{"id":{"externalId":null,"id":"3a3fe715-ef9e-4683-a932-7f67b2afbac0"},"state":"SHARED","serviceName":"Tele Medicine","serviceData":{"test":"42"},"price":25,"initiatorX500":"O=Company-B,L=Winterthur,ST=ZH,C=CH","serviceProviderX500":"O=Company-D,L=Zurich,ST=ZH,C=CH","linearId":{"externalId":null,"id":"3a3fe715-ef9e-4683-a932-7f67b2afbac0"}},"links":{"UPDATE":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/UPDATE","WITHDRAW":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/WITHDRAW","SEND_PAYMENT":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/SEND_PAYMENT","ACCEPT":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/ACCEPT","DECLINE":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0/DECLINE","self":"http://localhost:10804/api/v1/cordalo/template/services/3a3fe715-ef9e-4683-a932-7f67b2afbac0"},"error":null},{"state":{"id":{"externalId":null,"id":"fc6f8a5f-dee3-48ad-a245-594e1b631cc9"},"state":"SHARED","serviceName":"New Service","serviceData":{},"price":34,"initiatorX500":"O=Company-A,L=Zurich,ST=ZH,C=CH","serviceProviderX500":"O=Company-D,L=Zurich,ST=ZH,C=CH","linearId":{"externalId":null,"id":"fc6f8a5f-dee3-48ad-a245-594e1b631cc9"}},"links":{"UPDATE":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/UPDATE","WITHDRAW":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/WITHDRAW","SEND_PAYMENT":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/SEND_PAYMENT","ACCEPT":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/ACCEPT","DECLINE":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9/DECLINE","self":"http://localhost:10804/api/v1/cordalo/template/services/fc6f8a5f-dee3-48ad-a245-594e1b631cc9"},"error":null}],
-    "/api/v1/cordalo/template/me" : {"me":{"commonName":null,"organisationUnit":null,"organisation":"Company-D","locality":"Zurich","state":"ZH","country":"CH","x500Principal":{"name":"O=Company-D,L=Zurich,ST=ZH,C=CH","encoded":"ME4xCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaSDEPMA0GA1UEBwwGWnVyaWNoMSEwHwYDVQQKDBhTd2lzc2NhbnRvIFBlbnNpb25zIEx0ZC4="}}},
-    "/api/v1/cordalo/template/peers" : {"peers":[{"commonName":null,"organisationUnit":null,"organisation":"Company-A","locality":"Zurich","state":"ZH","country":"CH","x500Principal":{"name":"O=Company-A,L=Zurich,ST=ZH,C=CH","encoded":"MEUxCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaSDEPMA0GA1UEBwwGWnVyaWNoMRgwFgYDVQQKDA9Td2lzcyBMaWZlIEx0ZC4="}},{"commonName":null,"organisationUnit":null,"organisation":"Company-B","locality":"Winterthur","state":"ZH","country":"CH","x500Principal":{"name":"O=Company-B,L=Winterthur,ST=ZH,C=CH","encoded":"MEYxCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaSDETMBEGA1UEBwwKV2ludGVydGh1cjEVMBMGA1UECgwMQVhBIExlYmVuIEFH"}},{"commonName":null,"organisationUnit":null,"organisation":"Company-D","locality":"Zug","state":"ZG","country":"CH","x500Principal":{"name":"O=Company-C,L=Zug,ST=ZG,C=CH","encoded":"MDYxCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaRzEMMAoGA1UEBwwDWnVnMQwwCgYDVQQKDANGWkw="}}]}
-}
-
-function API_Failed() {
-    this.f;
-}
-
-API_Failed.prototype.fail = function(f) {
-    this.f = f;
-}
-
-jQuery.fn.fail = function(f) {
-    var o = $(this[0]) // This is the element
-    f("missing mock for "+o);
-    return this; // This is needed so other functions can keep chaining off of this
-};
-
-
-
-function $get(object) {
-    if (local == "true") {
-        var api = findAPI(object.url);
-        if (api && MOCK[api]) {
-            console.log("successful MOCK for url "+object.url);
-            console.log(MOCK[api]);
-            object.success(MOCK[api]);
-        } else {
-            console.log("missing MOCK for url "+object.url);
-        }
-        return $(API_Failed());
-    }
-    return $.get(object);
-}
+cordaloEnv.addMock(
+    "/api/v1/cordalo/template/me",
+    {"me":{"commonName":null,"organisationUnit":null,"organisation":"Company-D","locality":"Zurich","state":"ZH","country":"CH","x500Principal":{"name":"O=Company-D,L=Zurich,ST=ZH,C=CH","encoded":"ME4xCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaSDEPMA0GA1UEBwwGWnVyaWNoMSEwHwYDVQQKDBhTd2lzc2NhbnRvIFBlbnNpb25zIEx0ZC4="}}}
+);
+cordaloEnv.addMock(
+    "/api/v1/cordalo/template/peers",
+    {"peers":[{"commonName":null,"organisationUnit":null,"organisation":"Company-A","locality":"Zurich","state":"ZH","country":"CH","x500Principal":{"name":"O=Company-A,L=Zurich,ST=ZH,C=CH","encoded":"MEUxCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaSDEPMA0GA1UEBwwGWnVyaWNoMRgwFgYDVQQKDA9Td2lzcyBMaWZlIEx0ZC4="}},{"commonName":null,"organisationUnit":null,"organisation":"Company-B","locality":"Winterthur","state":"ZH","country":"CH","x500Principal":{"name":"O=Company-B,L=Winterthur,ST=ZH,C=CH","encoded":"MEYxCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaSDETMBEGA1UEBwwKV2ludGVydGh1cjEVMBMGA1UECgwMQVhBIExlYmVuIEFH"}},{"commonName":null,"organisationUnit":null,"organisation":"Company-D","locality":"Zug","state":"ZG","country":"CH","x500Principal":{"name":"O=Company-C,L=Zug,ST=ZG,C=CH","encoded":"MDYxCzAJBgNVBAYTAkNIMQswCQYDVQQIDAJaRzEMMAoGA1UEBwwDWnVnMQwwCgYDVQQKDANGWkw="}}]}
+);
