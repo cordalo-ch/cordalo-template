715b653f75c57303ec4dbe847fffccd309b038c1
Lorenz Hänggi <lolo8304@gmail.com>
2019-11-10 19:06:29 +0100

    implement / for index.html

--- clients/src/main/java/com/cordalo/template/client/webserver/WebConfig.java
+++ clients/src/main/java/com/cordalo/template/client/webserver/WebConfig.java
@@ -1,10 +1,7 @@
 package com.cordalo.template.client.webserver;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.resource.GzipResourceResolver;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
@@ -42,5 +39,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .setCachePeriod( 3600 )
                 .resourceChain(true) // 4.1
                 .addResolver(new GzipResourceResolver()) // 4.1
-                .addResolver(new PathResourceResolver());    }
+                .addResolver(new PathResourceResolver());
+    }
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:/index.html");
+    }
 }
