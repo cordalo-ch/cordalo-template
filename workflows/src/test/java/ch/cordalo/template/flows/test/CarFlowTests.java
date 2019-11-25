ec59ce23e4175244d1360fba76c220f0db4255c6
Cédric Walter <cedric.walter@gmail.com>
2019-11-25 09:36:48 +0100

    format and update copyright

--- workflows/src/test/java/ch/cordalo/template/flows/test/CarFlowTests.java
+++ workflows/src/test/java/ch/cordalo/template/flows/test/CarFlowTests.java
@@ -1,25 +1,28 @@
+/*
+ * Copyright (c) 2019 by cordalo.ch - MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package ch.cordalo.template.flows.test;
 
-import ch.cordalo.corda.common.contracts.StateVerifier;
 import ch.cordalo.corda.common.test.CordaNodeEnvironment;
 import ch.cordalo.template.flows.CarFlow;
-import ch.cordalo.template.flows.ChatMessageFlow;
 import ch.cordalo.template.states.CarState;
-import co.paralleluniverse.fibers.Suspendable;
-import net.corda.core.concurrent.CordaFuture;
-import net.corda.core.contracts.ContractState;
 import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.flows.FlowException;
 import net.corda.core.flows.FlowLogic;
 import net.corda.core.transactions.SignedTransaction;
-import org.assertj.core.internal.cglib.asm.$Type;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
 public class CarFlowTests extends CordaloTemplateBaseFlowTests {
 
