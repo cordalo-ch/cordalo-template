81772f148b06ab474ae7610f94338a8e2f0e83d4
Cédric Walter <cedric.walter@gmail.com>
2019-11-26 13:00:33 +0100

    use absolute path

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,10 +5,10 @@ export CORDA_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && p
 export NodeNames=`grep ".*O=.*L=.*C=.*" $CORDA_HOME/build.gradle | awk -F "," '{print $1}' | awk -F "=" '{print $2}' | awk -F " " '{print $1}'`
 export NodeNof=`grep ".*O=.*L=.*C=.*" $CORDA_HOME/build.gradle | awk -F "," '{print $1}' | awk -F "=" '{print $2}' | awk -F " " '{print $1}' | wc -l`
 
-export NodeSSHPorts=`grep ssh ../build.gradle | awk '{print $2}'`
+export NodeSSHPorts=`grep ssh $CORDA_HOME/build.gradle | awk '{print $2}'`
 export NodeSSHPorts=`echo $NodeSSHPorts | awk '{gsub(" ","|"); print}'`
 
-export NodeWebPorts=`grep server.port ../clients/build.gradle | awk -F "'" '{print $2}' | awk -F "=" '{print $2}'`
+export NodeWebPorts=`grep server.port $CORDA_HOME/clients/build.gradle | awk -F "'" '{print $2}' | awk -F "=" '{print $2}'`
 export NodeWebPorts=`echo $NodeWebPorts | awk '{gsub(" ","|"); print}'`
 
 export NodeSSHPortStart=`echo $NodeSSHPorts | awk -F "|" '{print $1}'`
